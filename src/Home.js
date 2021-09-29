539ed3c9080138f99495d0ac1295797246169e32
Sylek <sylek38@gmail.com>
2021-09-29 19:50:40 +0200

    add react-route-dom

--- src/Home.js
+++ src/Home.js
@@ -1,36 +1,9 @@
-import { useState, useEffect } from "react";
 import BlogList from "./BlogList";
+import useFetch from "./useFetch";
 
 const Home = () => {
 
-    const [blogs, setBlogs] = useState(null);
-    const [isPending, setIsPending] = useState(true);
-    const [error, setError] = useState(null);
-    
-    useEffect(() => {
-        setTimeout(() => {
-            fetch("http://localhost:8000/blogs")
-                .then(res => {
-                    if (!res.ok) {
-                        throw Error("could not fetch the data for that resource");
-                    }
-                    return res.json()
-                })
-                .then((blogsData) => {
-                    setBlogs(blogsData);
-                    setIsPending(false);
-                    setError(null);
-                })
-                .catch((err) => {
-                    setIsPending(false);
-                    setError(err.message);
-                    
-                })
-        }, 1000);
-    }, []);
-
-    // !
-    // [] dependency for useEffect - it makes useEffect run just once after one initial render
+    const { data: blogs, isPending, error } = useFetch("http://localhost:8000/blogs");
     
     return ( 
         <div className="home">
@@ -42,3 +15,5 @@ const Home = () => {
 }
  
 export default Home;
+
+// route parameters #25
