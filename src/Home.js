e7f49ce2f85377fcf7491550e41aa202311079ab
Sylek <sylek38@gmail.com>
2021-09-29 18:00:31 +0200

    add data handling

--- src/Home.js
+++ src/Home.js
@@ -3,19 +3,30 @@ import BlogList from "./BlogList";
 
 const Home = () => {
 
-    const [blogs, setBlogs] = useState([
-        { title: 'My new website', body: 'lorem ipsum...', author: 'mario', id: 1 },
-        { title: 'Welcome party!', body: 'lorem ipsum...', author: 'yoshi', id: 2 },
-        { title: 'Web dev top tips', body: 'lorem ipsum...', author: 'mario', id: 3 },
-    ]);
-
-    const handleDelete = (id) => {
-        const newBlogs = blogs.filter(blog => blog.id !== id);
-        setBlogs(newBlogs);
-    }
+    const [blogs, setBlogs] = useState(null);
+    const [isPending, setIsPending] = useState(true);
+    const [error, setError] = useState(null);
     
     useEffect(() => {
-        console.log("use effect ran");
+        setTimeout(() => {
+            fetch("http://localhost:8000/blogs")
+                .then(res => {
+                    if (!res.ok) {
+                        throw Error("could not fetch the data for that resource");
+                    }
+                    return res.json()
+                })
+                .then((blogsData) => {
+                    setBlogs(blogsData);
+                    setIsPending(false);
+                    setError(null);
+                })
+                .catch((err) => {
+                    setIsPending(false);
+                    setError(err.message);
+                    
+                })
+        }, 1000);
     }, []);
 
     // !
@@ -23,7 +34,9 @@ const Home = () => {
     
     return ( 
         <div className="home">
-            <BlogList blogs={blogs} title="All Blogs" handleDelete={handleDelete}/>
+            {error && <div>{ error }</div>}
+            {isPending && <div>Loading...</div>}
+            {blogs && <BlogList blogs={blogs} title="All Blogs"/>}
         </div>
      );
 }
